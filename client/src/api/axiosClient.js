03551f9c4d0304da789bef565ed4a07f0e6ac171
sd <superden125@gmail.comgit config --global user.name superden125>
2020-09-02 11:00:01 +0700

    fix url react

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -5,7 +5,8 @@ import queryString from "query-string";
 // Please have a look at here `https://github.com/axios/axios#request-config` for the full list of configs
 
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:
+    process.env.REACT_APP_API_URL_HEROKU || process.env.REACT_APP_API_URL,
   headers: {
     "content-type": "application/json",
   },
